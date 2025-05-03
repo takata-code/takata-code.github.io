c6c96f5e2ff8a9eb3a9d15aa3850764aa79095e3
takata-code <158656099+takata-code@users.noreply.github.com>
2025-05-03 17:30:33 +0900

    index.js を更新

---
+++ index.js
@@ -0,0 +1,14 @@
+//-------
+// Main
+//-------
+
+const projectEditorGetStarted = document.getElementById("projectEditorGetStarted")
+const projectChartGetStarted = document.getElementById("projectChartGetStarted")
+
+projectEditorGetStarted.addEventListener("click", () => {
+  location.href = "https://takaon.net/editor"
+})
+
+projectChartGetStarted.addEventListener("click", () => {
+  location.href = "https://takaon.net/chart"
+})
